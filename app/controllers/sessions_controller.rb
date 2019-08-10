09dc8a077a5915c0ff2a621304f8a483992c4776
github username <github email address>
2019-08-10 10:44:03 -0400

    took out byebug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:username])
-        byebug
         if @user && @user.authenticate(params[:password])
             token = JWT.encode({ user_id: @user.id }, '1234', 'HS256')
             render json: { token: token }, status: :ok
