d0fc5f6fd8f174266275f391e89aba5ef2291fa0
github username <github email address>
2019-08-19 16:57:25 -0400

    change cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://frontend-fitness-guru.firebaseapp.com'
-    origins '*'
+    origins 'https://frontend-fitness-guru.firebaseapp.com'
+    # origins '*'
 
     resource '*',
       headers: :any,
